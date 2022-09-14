d4b53a031122c7aa596fe32dec95e3c52c96ec7b
eloyball <eloyballesteros@hotmail.com>
2022-09-14 16:11:41 -0400

    init

---
+++ script.js
@@ -0,0 +1,32 @@
+// "Render" text as audio
+function renderTTS(text) {
+  var synthesis = window.speechSynthesis;
+
+  console.log("Available Voices:");
+  var voice = synthesis.getVoices().filter(function (voice) {
+    console.log(voice.name + " " + voice.lang);
+    return voice.name === 'Google UK English Female';
+  })[0];
+
+  // Create an utterance object
+  var utterance = new SpeechSynthesisUtterance(text);
+
+  // Set utterance properties
+  utterance.voice = voice;
+  utterance.pitch = 1;
+  utterance.rate = 1;
+  utterance.volume = 1;
+
+  // Speak the utterance
+  synthesis.speak(utterance);
+}
+
+function encourage() {
+  let name = document.getElementById("name").value;
+  let encouragement = document.getElementById("encouragement").value;
+  let postScript = document.getElementById("post-script").value;
+
+  let message = `Hello ${name}, I have something I would like to tell you. ${encouragement}. Have a nice day! ${postScript}`;
+
+  renderTTS(message);
+}
